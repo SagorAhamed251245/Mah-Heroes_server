370dab21df06bd8ea15cca563892e63866d231d5
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-20 17:05:23 +0600

    serch by toy name route done

--- index.js
+++ index.js
@@ -37,13 +37,41 @@ async function run() {
 
     const productsCollection = client.db('mahHeroes').collection('products');
 
+    const indexKeys = { toy_name: 1 };
+    const indexOptions = { name: "toyName" };
+
+    try {
+      const result = await productsCollection.createIndex(indexKeys, indexOptions);
+      console.log(result);
+    } catch (error) {
+      console.error("Error creating index:", error);
+    }
+
+   
+
     app.get('/products', async (req, res) => {
+      console.log(req.body);
       const products = await productsCollection.find().toArray();
       res.send(products);
     })
+
+    app.get("/getToyByName/:text", async (req, res) => {
+      const text = req.params.text;
+      console.log(text);
+      try {
+        const result = await productsCollection
+          .find({ toy_name: { $regex: text, $options: "i" } })
+          .toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error retrieving data:", error);
+        res.status(500).send("Error retrieving data");
+      }
+    });
+
     app.get('/products/limit', async (req, res) => {
 
-      const products = await productsCollection.find().limit(20).sort({ price: -1 }).toArray();
+      const products = await productsCollection.find().limit(20).toArray();
       res.send(products)
 
     })
@@ -55,41 +83,79 @@ async function run() {
       res.send(product);
     })
 
-    app.get('/mytoys/:email', async (req, res) => {
+    app.get('/mytoys/:email/:sort', async (req, res) => {
       const myEmail = req.params.email;
-      const result = await productsCollection.find({ seller_email: myEmail }).toArray()
-      res.send(result)
+      const sorting = req.params.sort;
+
+      console.log(typeof myEmail, myEmail);
+      console.log(typeof sorting, sorting);
+
+
+      /* console.log(sorting);
       
+      if(sorting==true){
+       const result = await productsCollection.find({ seller_email: myEmail }).sort({price: -1}).toArray()
+       res.send(result)
+      }
+      else{
+       const result = await productsCollection.find({ seller_email: myEmail }).sort({ price: 1 }).toArray()
+       
+       res.send(result)
+      } */
+
+      try {
+        let result;
+        result = await productsCollection
+          .find({ seller_email: myEmail })
+          .sort({ price: sorting === 'true' ? -1 : 1 })
+          .toArray()
+
+
+
+        // Convert the price field to a number
+        result.forEach((toy) => {
+          toy.price = parseInt(toy.price)
+          console.log(toy.price);;
+        })
+          ;
+
+        res.send(result);
+      } catch (error) {
+        console.error('Error retrieving toys:', error);
+        res.status(500).json({ error: 'Error retrieving toys' });
+      }
+
+
     })
-    
+
 
     app.post('/addProduct', async (req, res) => {
       const body = req.body;
       const result = await productsCollection.insertOne(body)
       res.send(result)
-      
+
     })
-    app.patch('/updateToy/:id', async(req , res)=> {
-      const id = req.params.id ;
-      const filter = {_id: new ObjectId(id)}
-      const option = { upsert: true}
+    app.patch('/updateToy/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const option = { upsert: true }
       const body = req.body
       const updateToyInfo = {
         $set: {
-          price: body.price ,
-          available_quantity: body.available_quantity ,
-          description: body.description 
+          price: body.price,
+          available_quantity: body.available_quantity,
+          description: body.description
 
         }
       }
 
-      const result = await productsCollection.updateOne(filter , updateToyInfo , option)
+      const result = await productsCollection.updateOne(filter, updateToyInfo, option)
       res.send(result)
-      
+
     })
-    app.delete('/deleteProduct/:id' , async(req , res)=> {
-      const id = req.params.id ;
-      const result = await productsCollection.deleteOne({_id: new ObjectId(id)})
+    app.delete('/deleteProduct/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await productsCollection.deleteOne({ _id: new ObjectId(id) })
       res.send(result)
     })
 
