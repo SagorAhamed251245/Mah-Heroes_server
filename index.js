0f9343506a9147fd89086214781c3299f9bf3cb3
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-21 12:49:24 +0600

    some modify

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+
       client.connect((err) => {
         if (err){
           console.error(err);
