90d31d8f91b2af99ad21b2b8ee3926dcb030b8ad
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-16 18:01:25 +0600

    fix the error

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
   },
-  useNewUrlParser : true ,
-  useUnifiedTopology : true , 
-  maxPoolSize : 10
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
